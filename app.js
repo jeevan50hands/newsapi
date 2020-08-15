01fdec3aae4e32f25dec7b7f13354323d08d228d
jeevan50hands <66291401+jeevan50hands@users.noreply.github.com>
2020-08-15 18:19:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const request = require('postman-request')
+const fs = require('fs')
+const express = require('express')
+const port = process.env.PORT || 3000
+const app = express()
+const news = fs.readFileSync('./jobs_news.json')
+
+const GetNewsFromAPI = () => {
+    fs.unlinkSync("jobs_news.json")
+    url = "http://newsapi.org/v2/everything?q=Jobs&pageSize=100&sortBy=publishedAt&apiKey=5f84106dae3d475092fb76d3b5f148e5"
+    request({ url, 'json': true }, (error, { body }) => {
+        if (error) {
+            console.log(error);
+        } else if (body.error) {
+            console.log(body.error);
+        } else {
+            fs.writeFileSync('jobs_news.json', JSON.stringify({ NEWS: body.articles }))
+        }
+    })
+}
+
+setInterval(() => {
+    GetNewsFromAPI()
+}, 3600000);
+
+app.get('/jobnews', (req, res) => {
+    return res.send(JSON.parse(news))
+})
+
+app.get('*', (req, res) => {
+    return res.render({
+        errorText: '404 Error'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
+})
