8b0a26af197db8d2b1652ecb1b0659392bc95c14
jeevan50hands <66291401+jeevan50hands@users.noreply.github.com>
2020-08-16 18:39:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const request = require('postman-request')
 const fs = require('fs')
 const express = require('express')
-
 const port = process.env.PORT || 3000
 const app = express()
-const news = fs.readFileSync('jobs_news.json')
-    // const news = { NEWS: '' }
+    // const news = fs.readFileSync('jobs_news.json')
+const news = { NEWS: '' }
 
 const GetNewsFromAPI = () => {
     console.log('i am running');
-    fs.unlinkSync("jobs_news.json")
+    // fs.unlinkSync("jobs_news.json")
     url = "http://newsapi.org/v2/everything?q=Jobs&pageSize=100&sortBy=publishedAt&apiKey=5f84106dae3d475092fb76d3b5f148e5"
     request({ url, 'json': true }, (error, { body }) => {
         if (error) {
@@ -17,7 +16,8 @@ const GetNewsFromAPI = () => {
         } else if (body.error) {
             console.log(body.error);
         } else {
-            fs.writeFileSync('jobs_news.json', JSON.stringify({ NEWS: body.articles }))
+            // fs.writeFileSync('jobs_news.json', JSON.stringify({ NEWS: body.articles }))
+            news.NEWS = body.articles
         }
     })
 }
@@ -27,7 +27,9 @@ setInterval(() => {
 }, 10000);
 
 app.get('/', (req, res) => {
-    return res.send(JSON.parse(news))
+    // return res.send(JSON.parse(news))
+    return res.send(news)
+
 })
 
 app.listen(port, () => {
