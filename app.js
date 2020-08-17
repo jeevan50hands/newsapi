92f9c8f131daa5fe4932ee7dd40f81ebc2a921e4
jeevan50hands <66291401+jeevan50hands@users.noreply.github.com>
2020-08-17 07:00:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,35 +8,30 @@ const app = express()
 const news = { NEWS: '' }
 
 const GetNewsFromAPI = () => {
-    // fs.unlinkSync("jobs_news.json")
-    console.log('i am running');
-    url = "http://newsapi.org/v2/everything?q=Jobs&pageSize=100&sortBy=publishedAt&apiKey=5f84106dae3d475092fb76d3b5f148e5"
-    request({ url, 'json': true }, (error, { body }) => {
-        if (error) {
-            console.log(error);
-        } else if (body.error) {
-            console.log(body.error);
-        } else {
-            // fs.writeFileSync('jobs_news.json', JSON.stringify({ NEWS: body.articles }))
-            news.NEWS = body.articles
-        }
-    })
-}
-a = 0
+        // fs.unlinkSync("jobs_news.json")
+        url = "http://newsapi.org/v2/everything?q=Jobs&pageSize=100&sortBy=publishedAt&apiKey=5f84106dae3d475092fb76d3b5f148e5"
+        request({ url, 'json': true }, (error, { body }) => {
+            if (error) {
+                console.log(error);
+            } else if (body.error) {
+                console.log(body.error);
+            } else {
+                // fs.writeFileSync('jobs_news.json', JSON.stringify({ NEWS: body.articles }))
+                news.NEWS = body.articles
+            }
+        })
+    }
     // schedule.scheduleJob('*/1 * * * *', function() {
     //     GetNewsFromAPI()
     // });
 
-let v = schedule.scheduleJob('* */1 * * * *', function() {
-    return a += 1
-});
-
-
+setInterval(() => {
+    GetNewsFromAPI()
+}, 1000);
 
 app.get('/', (req, res) => {
     // return res.send(JSON.parse(news))
-    // return res.send(news)
-    return res.send(JSON.stringify({ number: a }))
+    return res.send(news)
 
 })
 
